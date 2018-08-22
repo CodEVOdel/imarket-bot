94198c1906fda64096a03aa2510ee526219e61b8
Nikolay <avakyan.nikolay@gmail.com>
2018-08-22 16:34:26 +0500

    обновление скрипта

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const TOKKEN = '607358569:AAG5B4UN08H3Sqkjw0HhVIOnYO4JGFdZl5g';
 
 const bot = new TelegramBot(TOKKEN,{polling:true});
 
+var optional = {
+    parse_mode : "markdown",
+    disable_web_page_preview:false,
+    reply_markup: JSON.stringify({
+        inline_keyboard:[
+            [{text:'Телефон'}],
+            [{text:'Телефон'}]
+        ]
+    })
+};
+
+
 bot.on('message',msg=>{
     if (msg.text=="/start") {
         bot.sendMessage(msg.chat.id,'Привет '+msg.from.first_name);
     }
     if (msg.text=="/finish") {
         bot.sendMessage(msg.chat.id,'Пока');
+    }
+    if (msg.text=="/btm") {
+        bot.sendMessage(msg.chat.id,'Кнопка',optional);
     }
 });
