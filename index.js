a0377fc47e79b73270f69c60f6d8dab3c243fdc5
Nikolay <avakyan.nikolay@gmail.com>
2018-08-22 15:58:42 +0500

    обновление скрипта

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const TelegramBot = require('node-telegram-bot-api');
 
 const TOKKEN = '607358569:AAG5B4UN08H3Sqkjw0HhVIOnYO4JGFdZl5g';
@@ -6,5 +5,10 @@ const TOKKEN = '607358569:AAG5B4UN08H3Sqkjw0HhVIOnYO4JGFdZl5g';
 const bot = new TelegramBot(TOKKEN,{polling:true});
 
 bot.on('message',msg=>{
-    bot.sendMessage(msg.chat.id,'спааааать');
+    if (msg.text=="/start") {
+        bot.sendMessage(msg.chat.id,'Привет '+msg.from.first_name);
+    }
+    if (msg.text=="/finish") {
+        bot.sendMessage(msg.chat.id,'Пока');
+    }
 });
